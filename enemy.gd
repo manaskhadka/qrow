8e920396459445008595fd2d237dea8df1228eb5
Manas Khadka <manazkhadka@gmail.com>
2024-05-21 11:29:40 -0700

    Outdoor area and basic enemy

---
+++ enemy.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+@export var slow_coeff = 100
+@export var player_chase = false
+var player = null
+var original_pos
+
+var health = 100
+var player_inattack_zone = false
+
+
+func _ready():
+	original_pos = self.position
+	
+func _physics_process(delta):
+	process_damage()
+	if player_chase:
+		position += (player.position - self.position) / slow_coeff
+	else:
+		position += (original_pos - self.position) / slow_coeff
+		
+	
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
+
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_inattack_zone = false  
+		
+func process_damage():
+	if player_inattack_zone and global.player_current_attack:
+		health = health - 20  
+		print("enemy health:", health)
+		if health <= 0:
+			self.queue_free()
+	
--- player.gd
+++ player.gd
@@ -1,24 +1,41 @@
 extends CharacterBody2D
 
-
-@export var decel_coeff: float = .88
-@export var speed: float = 10
-@export var max_speed: float = 100
+@export var decel_coeff: float = .92
+@export var speed: float = 15
+@export var max_speed: float = 150
+var enemy_in_attack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+var curr_direction = "right"
+var is_attacking = false
 
 @onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D as AnimatedSprite2D	
 
 func _physics_process(delta) -> void:
+	attack()
+	movement()
+	enemy_attack()
+	if health <= 0:
+		print("player has died")
+		health = 100
+
+func movement():
 	if Input.is_action_pressed("up"):
+		curr_direction = "up"
 		if (velocity.y >= -1*max_speed):
 			velocity.y -= speed
 	if Input.is_action_pressed("down"):
+		curr_direction = "down"
 		if (velocity.y <= max_speed):
 			velocity.y += speed
 	if Input.is_action_pressed("left"):
+		curr_direction = "left"
 		if (velocity.x >= -1*max_speed):
 			velocity.x -= speed
 		anim_sprite.play("flying left")
 	if Input.is_action_pressed("right"):
+		curr_direction = "right"
 		if (velocity.x <= max_speed):
 			velocity.x += speed
 		anim_sprite.play("flying right")
@@ -27,4 +44,50 @@ func _physics_process(delta) -> void:
 	velocity.x *= decel_coeff
 	
 	move_and_slide()
+	
+func attack():
+	var dir = curr_direction 
+	
+	if Input.is_action_pressed("attack"):
+		if is_attacking == false:
+			global.player_current_attack = true
+			is_attacking = true
+			if curr_direction == "right":
+				# handle animations
+				pass 
+			if curr_direction == "left":
+				# handle animations
+				pass
+			$"attack-cooldown".start()
+	
+	
+func player():
+	pass
+	
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"): 
+		enemy_in_attack_range = true
 
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_in_attack_range = false
+		
+func enemy_attack():
+	if enemy_in_attack_range and enemy_attack_cooldown:
+		enemy_attack_cooldown = false
+		$"invincibility-cooldown".start()
+		health -= 10
+		print("Health:", health)
+
+
+func _on_invincibilitycooldown_timeout():
+	# This gets called whenever the invincibility timer goes off
+	enemy_attack_cooldown = true
+
+
+func _on_attackcooldown_timeout():
+	print("Player attacked!")
+	$"attack-cooldown".stop()
+	global.player_current_attack = false 
+	is_attacking = false
