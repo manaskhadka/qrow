88c92abd17dba2eebaadc386a87f3732c8c6852c
EmilyMacias <99148529+EmilyMacias@users.noreply.github.com>
2024-05-26 14:35:13 -0700

    witch lights up when near and stops lighting up when you move away

--- Area2D2.gd
+++ Area2D2.gd
@@ -8,13 +8,10 @@ func _on_body_entered(body):
 
 func _on_body_exited(body):
 	entered = false # Replace with function body.
+	find_child("Witch").material.set_shader_parameter("outline_enabled", false)
 
 func _process(delta):
 	if entered == true:
-		print("trying to talk to witch")
-		print(	find_child("Witch"))
-		print(find_child("Witch").material.get_shader_parameter("outline_enabled"))
 		find_child("Witch").material.set_shader_parameter("outline_enabled", true)
-		print(find_child("Witch").material.get_shader_parameter("outline_enabled"))
 		if Input.is_action_just_pressed("ui_accept"):
 			get_tree().change_scene_to_file("res://world4.tscn")
