def23eb2353f0d9ea87f0fac853cb3e80939dcac
EmilyMacias <99148529+EmilyMacias@users.noreply.github.com>
2024-05-27 18:00:49 -0700

    progress on reusable element, can change image through code

---
+++ interacteable_script.gd
@@ -0,0 +1,23 @@
+extends Area2D
+var entered = false
+@export var image : Texture
+
+func _ready():
+	$interactive_object.texture = preload("res://5.png")
+		
+func _on_body_entered(body):
+	entered = true # Replace with function body.
+	print("I am close")
+
+
+func _on_body_exited(body):
+	entered = false # Replace with function body.
+	print(find_child("interactive_object").material.get_shader_parameter("outline_enabled"))
+	find_child("interactive_object").material.set_shader_parameter("outline_enabled", false)
+	print(find_child("interactive_object").material.get_shader_parameter("outline_enabled"))
+
+func _process(delta):
+	if entered == true:
+		find_child("interactive_object").material.set_shader_parameter("outline_enabled", true)
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://world4.tscn")
