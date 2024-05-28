7cb958563a00404be8b3cbdb0788b4dac5b0138b
pjjasmine <pjasmine@stanford.edu>
2024-05-28 00:02:47 -0700

    Revert "Merge branch 'main' of https://github.com/manaskhadka/qrow"
    This reverts commit 4c3e30d7c158a6b298938235419d988a889b50e2, reversing
    changes made to f6cf368880662bfcf0030870a28afca23d114306.

--- Area2D2.gd
+++ Area2D2.gd
@@ -8,10 +8,8 @@ func _on_body_entered(body):
 
 func _on_body_exited(body):
 	entered = false # Replace with function body.
-	find_child("Witch").material.set_shader_parameter("outline_enabled", false)
 
 func _process(delta):
 	if entered == true:
-		find_child("Witch").material.set_shader_parameter("outline_enabled", true)
 		if Input.is_action_just_pressed("ui_accept"):
 			get_tree().change_scene_to_file("res://world4.tscn")
