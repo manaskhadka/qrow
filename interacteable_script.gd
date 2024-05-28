43d09651b8a1cfa1021efc0205bccfcdb0d3f210
pjjasmine <pjasmine@stanford.edu>
2024-05-28 00:07:39 -0700

    Reapply "Merge branch 'main' of https://github.com/manaskhadka/qrow"
    This reverts commit 7cb958563a00404be8b3cbdb0788b4dac5b0138b.

--- interacteable_script.gd
+++ interacteable_script.gd
@@ -1,17 +1,19 @@
 extends Area2D
 var entered = false
+@export var image : String = "res://graphics/Top-Down Retro Interior/Shelf.png"
 
-
+func _ready():
+	$object.texture = load(image)
+		
 func _on_body_entered(body):
 	entered = true # Replace with function body.
-
-
+	
 func _on_body_exited(body):
 	entered = false # Replace with function body.
-	find_child("Witch").material.set_shader_parameter("outline_enabled", false)
+	find_child("object").material.set_shader_parameter("outline_enabled", false)
 
 func _process(delta):
 	if entered == true:
-		find_child("Witch").material.set_shader_parameter("outline_enabled", true)
+		find_child("object").material.set_shader_parameter("outline_enabled", true)
 		if Input.is_action_just_pressed("ui_accept"):
 			get_tree().change_scene_to_file("res://world4.tscn")
