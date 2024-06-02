b7e6faa4d8a12bedae7c056fef3af869cea33502
EmilyMacias <99148529+EmilyMacias@users.noreply.github.com>
2024-06-01 17:17:22 -0700

    inventory item sort of shows up, but too big

--- inventory.gd
+++ inventory.gd
@@ -1,10 +1,11 @@
 extends Node2D
 var is_open = false
-
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	close() # Replace with function body.
+	
 func open():
 	self.visible = true
 	is_open = true
@@ -15,7 +16,15 @@ func close():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("i"):
+		print("minimum size")
+		var min = min(global.items.size(), slots.size())
+		print(min)
 		print("i")
+		for i in range(min):
+			print("slot")
+			print(slots[i])
+			slots[i].update(global.items[i])
+			
 		if is_open:
 			close()
 		else:
---
+++ panel.gd
@@ -0,0 +1,23 @@
+extends Panel
+
+@onready var backgroundSprite: Sprite2D = $background
+@onready var itemSprite: Sprite2D = $item
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func update(item2: Sprite2D):
+	print("update was called")
+	if !item2:
+		backgroundSprite.frame = 0
+		itemSprite.visible = false
+	else:
+		backgroundSprite.frame = 1
+		itemSprite.visible = true
+		print("show image")
+		$item.texture = item2.texture
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
